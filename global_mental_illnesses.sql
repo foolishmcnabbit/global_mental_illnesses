efc33384596f9093ffe516bda32fd2f3a76657d6
David Stone <136173430+foolishmcnabbit@users.noreply.github.com>
2024-05-30 20:03:32 -0400

    Update global_mental_illnesses.sql
    cleaned up the code to look better

--- global_mental_illnesses.sql
+++ global_mental_illnesses.sql
@@ -15,48 +15,48 @@ WITH schizophrenia AS (
 depressive AS (
 	SELECT
 		Country,
-        Year,
-        `Depressive disorders` AS max_value
+        	Year,
+        	`Depressive disorders` AS max_value
 	FROM mental_illnesses
-    ORDER BY `Depressive disorders` DESC
-    LIMIT 1
+    	ORDER BY `Depressive disorders` DESC
+    	LIMIT 1
 ),
 anxiety AS (
 	SELECT
 		Country,
-        Year,
-        `Anxiety disorders` AS max_value
+        	Year,
+        	`Anxiety disorders` AS max_value
 	FROM mental_illnesses
-    ORDER BY `Anxiety disorders` DESC
-    LIMIT 1
+    	ORDER BY `Anxiety disorders` DESC
+    	LIMIT 1
 )
 SELECT
 	"Schizophrenia disorders" AS disorder,
-    schizophrenia.Country AS country,
-    schizophrenia.Year AS year,
-    schizophrenia.max_value
+    	schizophrenia.Country AS country,
+    	schizophrenia.Year AS year,
+    	schizophrenia.max_value
 FROM schizophrenia
 UNION ALL
 SELECT
 	"Depressive disorders",
-    depressive.Country,
-    depressive.Year,
-    depressive.max_value
+    	depressive.Country,
+    	depressive.Year,
+    	depressive.max_value
 FROM depressive
 UNION ALL
 SELECT
 	"Anxiety disorders",
-    anxiety.Country,
-    anxiety.Year,
-    anxiety.max_value
+    	anxiety.Country,
+    	anxiety.Year,
+    	anxiety.max_value
 FROM anxiety;
 
 -- time series of disorders in the USA
 SELECT
 	Year,
-    `Depressive disorders` AS depressive_disorders,
-    `Schizophrenia disorders` AS schizophrenia_disorders,
-    `Anxiety disorders` AS anxiety_disorders
+    	`Depressive disorders` AS depressive_disorders,
+    	`Schizophrenia disorders` AS schizophrenia_disorders,
+    	`Anxiety disorders` AS anxiety_disorders
 FROM mental_illnesses
 WHERE Country = "United States"
 ORDER BY Year DESC;
@@ -64,8 +64,8 @@ ORDER BY Year DESC;
 -- order the countries by the highest depressive disorder value
 SELECT
 	Country,
-    MAX(`Depressive disorders`) AS depressive_disorders,
-    RANK() OVER (ORDER BY MAX(`Depressive disorders`) DESC) AS disorder_rank
+    	MAX(`Depressive disorders`) AS depressive_disorders,
+    	RANK() OVER (ORDER BY MAX(`Depressive disorders`) DESC) AS disorder_rank
 FROM mental_illnesses
 GROUP BY Country
 ORDER BY MAX(`Depressive disorders`) DESC;
@@ -73,8 +73,8 @@ ORDER BY MAX(`Depressive disorders`) DESC;
 -- order the countries by the average depressive disorder value
 SELECT
 	Country,
-    AVG(`Depressive disorders`) AS depressive_disorders,
-    RANK() OVER (ORDER BY AVG(`Depressive disorders`) DESC) AS disorder_rank
+    	AVG(`Depressive disorders`) AS depressive_disorders,
+    	RANK() OVER (ORDER BY AVG(`Depressive disorders`) DESC) AS disorder_rank
 FROM mental_illnesses
 GROUP BY Country
 ORDER BY AVG(`Depressive disorders`) DESC;
